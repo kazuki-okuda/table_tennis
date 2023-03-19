b0aef4dae03917a362d9c019f1fd147b41dc4c45
Miomon3 <mio14.tana111@gmail.com>
2023-03-19 14:50:49 +0900

    ファイルの追加と一覧画面の作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,12 +5,36 @@
         <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="./css/index.css">
     </head>
+    
     <body>
-        <h1>チーム開発会へようこそ！</h1>
-        <h2>投稿一覧画面</h2>
-        <a href='/posts/create'>新規投稿</a>
-        <div>
+        <header>
+            <div class=headerContents>
+                <h1>友達プロフィール一覧</h1>
+                <nav>
+                    <ul>
+                        <li>My profile</li>
+                　　      <li>友達追加</li>
+            　　      </ul>
+        　　      </nav>
+            </div>
+    　　</header>
+        <body>
+            <div class=bodyContents>
+                <p class=new>新規作成</p>
+                <div class=profile>
+                    <h3>idAさん</h3>
+                    <p class=more>詳細</p>
+                </div>
+            </div>
+            <br>
+            <br>
+            <br>
+            <h1>チーム開発会へようこそ！</h1>
+            <h2>投稿一覧画面</h2>
+            <a href='/posts/create'>新規投稿</a>
+            <div>
             @foreach ($posts as $post)
                 <div style='border:solid 1px; margin-bottom: 10px;'>
                     <p>
@@ -19,9 +43,10 @@
                     <p>カテゴリー：<a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a></p>
                 </div>
             @endforeach
-        </div>
-        <div>
+            </div>
+            <div>
             {{ $posts->links() }}
-        </div>
+            </div>
+        </body>
     </body>
 </html>
