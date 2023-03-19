56e700ce9445dc79a3d42689ebe01003ede78575
erena0424 <er24en3a6@gmail.com>
2023-03-19 14:37:18 +0900

    profiles,friendsテーブルの追加

---
+++ database/migrations/2023_03_19_122352_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('image',100)->nullable();
+            $table->date('born')->nullable();
+            $table->string('hobby',20)->nullable();
+            $table->string('job',20)->nullable();
+            $table->string('text',100)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
